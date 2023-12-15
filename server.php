0d9b22b307ecb909fa62fd2ba7fde96803570d24
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2023-12-15 10:05:50 +0100

    add: key="index"

--- server.php
+++ server.php
@@ -2,10 +2,5 @@
 
 $string = file_get_contents("dischi.json");
 
-
-$list_disk = json_decode($string, true);
-
-$json = json_encode($list_disk);
-
 header("Content-Type: application/json");
 echo $json;
