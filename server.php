07cda4987e99de6f1da9fc3364d8fdb383f3ce18
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2023-12-15 10:10:13 +0100

    refactoring

--- server.php
+++ server.php
@@ -2,5 +2,10 @@
 
 $string = file_get_contents("dischi.json");
 
+
+$list_disk = json_decode($string, true);
+
+$json = json_encode($list_disk);
+
 header("Content-Type: application/json");
 echo $json;
